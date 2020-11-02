8f5247ccc0f7a4ad4ddbe5e253d062b449a64a8b
JaonMicle <jaonmicle@outlook.com>
2020-11-03 03:14:26 +0800

    firs commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var SSE = require('./libs/sse');
+
+
+module.exports = function (res, options) {
+
+    var newServerSent = new SSE(res, options);
+    
+    return newServerSent;
+};
+
+module.exports.SSEServer = SSE;
